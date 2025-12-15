6ccd4cb285a984a11783a6c4f9e67c0da3a29e10
Oğuz Özbal <130146855+ogzozbl@users.noreply.github.com>
2025-12-15 22:11:39 +0300

    Integrate Firebase Auth and ThemeService in app routing

--- lib/app.dart
+++ lib/app.dart
@@ -2,14 +2,38 @@ import 'package:flutter/material.dart';
 import 'screens/auth/login_screen.dart';
 import 'screens/commitly_home/commitly_home_screen.dart';
 import 'services/auth_service.dart';
+import 'services/theme_service.dart';
 
-class CommitlyApp extends StatelessWidget {
+class CommitlyApp extends StatefulWidget {
   const CommitlyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final authService = AuthService();
+  State<CommitlyApp> createState() => _CommitlyAppState();
+}
 
+class _CommitlyAppState extends State<CommitlyApp> {
+  final _themeService = ThemeService();
+  final _authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    _themeService.addListener(_onThemeChanged);
+  }
+
+  void _onThemeChanged() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _themeService.removeListener(_onThemeChanged);
+    _themeService.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Commitly',
       theme: ThemeData.light(useMaterial3: true).copyWith(
@@ -17,12 +41,19 @@ class CommitlyApp extends StatelessWidget {
           seedColor: Colors.purple,
           brightness: Brightness.light,
         ),
-        scaffoldBackgroundColor: Colors.grey.shade100, // Light grey background
+        scaffoldBackgroundColor: Colors.grey.shade100,
       ),
+      darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.dark,
+        ),
+        scaffoldBackgroundColor: const Color(0xFF121212),
+      ),
+      themeMode: _themeService.themeMode,
       home: StreamBuilder(
-        stream: authService.authStateChanges,
+        stream: _authService.authStateChanges,
         builder: (context, snapshot) {
-          // Show loading indicator while checking auth state
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Scaffold(
               body: Center(
@@ -31,11 +62,9 @@ class CommitlyApp extends StatelessWidget {
             );
           }
 
-          // If user is logged in, show home screen
-          // If user is logged out, show login screen
           final user = snapshot.data;
           if (user != null) {
-            return const CommitlyHomeScreen();
+            return CommitlyHomeScreen(themeService: _themeService);
           } else {
             return const LoginScreen();
           }
