feff05ffecf8f2b6afd7798011004e115651ba4a
Oğuz Özbal <130146855+ogzozbl@users.noreply.github.com>
2025-12-15 19:04:48 +0300

    Initialize Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   if (!kIsWeb &&
       (defaultTargetPlatform == TargetPlatform.windows ||
